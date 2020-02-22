0c3bc0960fa2d1b65a74596b2c0b55e1bd22a3cf
Fernando Cardenas <fernando.cardenas@grandcentrix.net>
2020-02-22 11:31:41 +0100

    Initial Commit

---
+++ MarvelFinalImplementation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelFinalImplementation
+//
+//  Created by Fernando Cardenas on 22.02.20.
+//  Copyright © 2020 fernandocardenasm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
